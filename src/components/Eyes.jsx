a05e2c380ab6618f0d6bb625588befe8a1fe2662
Md Kaif <mdkaif0153@gmail.com>
2025-08-21 18:32:29 +0530

    added playful feature of eyes

---
+++ src/components/Eyes.jsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react'
+
+function Eyes() {
+    const [rotate, setRotate] = useState(0)
+
+    useEffect(()=>{
+        window.addEventListener('mousemove',(e)=>{
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+
+            const deltaX = mouseX - window.innerWidth/2;
+            const deltaY = mouseY - window.innerHeight/2;
+
+            const angle = Math.atan2(deltaY, deltaX) * (180/Math.PI);
+            setRotate(angle-180);
+        })
+    })
+
+    return (
+      <div className="w-full h-screen bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg)] relative overflow-hidden">
+        <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] flex gap-5">
+          <div className="w-[15vw] h-[15vw] flex justify-center items-center rounded-full bg-zinc-100">
+            <div className="w-2/3 h-2/3 flex justify-center items-center rounded-full bg-zinc-800">
+              <div
+                className="line w-full"
+                style={{ transform: `rotate(${rotate}deg)` }}
+              >
+                <div className="w-6 h-6 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[15vw] h-[15vw] flex justify-center items-center rounded-full bg-zinc-100">
+            <div className="w-2/3 h-2/3 flex justify-center items-center rounded-full bg-zinc-800">
+              <div
+                className="line w-full"
+                style={{ transform: `rotate(${rotate}deg)` }}
+              >
+                <div className="w-6 h-6 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Eyes
