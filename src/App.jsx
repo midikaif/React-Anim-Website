7381fe316e7114345161adf2be25509c6928b36f
Md Kaif <mdkaif0153@gmail.com>
2025-09-22 20:19:55 +0530

    Added featured projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Eyes from './components/Eyes'
+import Featured from './components/Featured'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Marquee/>      
       <About/>
       <Eyes/>
+      <Featured/>
     </div>
   )
 }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div className="rounded-tl-3xl rounded-tr-3xl">
+      <div className="p-12 pt-30 text-[4vw] leading-none tracking-tight ">
+        Featured projects
+      </div>
+      <div className="border-t-1 border-zinc-400 flex gap-10 p-12">
+        <div className="w-1/2">
+          <div className="flex items-center uppercase">
+            <span className='before:content-["•"] before:mr-2 before:text-zinc-800 before:text-4xl'></span>
+            <span className="pt-1">Salience Labs</span>
+          </div>
+          <div className="h-[80vh] rounded-xl overflow-hidden bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2025/02/Salience_Website_cover-663x551.png)]"></div>
+          <div className="pt-4">
+            <button className="uppercase border-1 rounded-4xl px-2 py-1 leading-none tracking-tight ">brand identity</button>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="flex items-center uppercase">
+            <span className='before:content-["•"] before:mr-2 before:text-zinc-800 before:text-4xl'></span>
+            <span className="pt-1">Medallia Experience</span>
+          </div>
+          <div className="h-[80vh] rounded-xl overflow-hidden bg-cover bg-center bg-[url(https://ochi.design/wp-content/uploads/2025/08/Med_Website_0.png)]"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Featured
